6a67fc77fb0209fa1f50169ad06c6bd08b1e449a
MarllonCampos <marllondcsp@gmail.com>
2020-04-23 18:47:55 -0300

    Update em JS
    Colocado a função de troca de divs, na segunda pagina

--- Site - Projeto Aps/js/index.js
+++ Site - Projeto Aps/js/index.js
@@ -1,8 +1,56 @@
 let upper_right = document.getElementById("upper-right")
-function rightside(data){
-   upper_right.innerHTML=`
-    <p> Bom dia </p>
-    <hr>
-    <p> Coversando as <b>05:44h</b> da manhã</p>
+
+
+
+
+function leftside(){
+   document.body.innerHTML= `
+         <div class="container"> 
+              <div class="panels">
+                  <div class="leftpanel">x</div>
+                  <div class="rightpanel" onmouseover="rightside()">y</div>
+              </div>
+
+              <div class="container-content">
+                  <div class='content' id='upper-left'>a</div>    
+
+                  <div class='content'id='lower-left'>b</div>     
+
+                  <div class='content'id='center'>c</div>      
+
+                  <div class='content' id='upper-right'>d</div>      
+
+                  <div class='content' id='lower-right'>e</div>
+              </div>
+
+          </div>  
    `
 }
+
+
+
+
+
+
+
+function rightside(){
+   document.body.innerHTML=`
+            <div class="container"> 
+
+               <div class="panels">
+                   <div class="leftpanel" onmouseenter="leftside()">x</div>
+                   <div class="rightpanel" onmouseenter="rightside()">y</div>
+               </div>
+
+               <div class="container-content">
+
+                   <div class='content'id='center'>c</div>
+
+               </div>
+
+            </div>  
+`
+}
+
+
+
