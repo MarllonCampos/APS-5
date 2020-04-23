e81cc87f01efba32200736837e1b44f8e6fe3a86
MarllonCampos <marllondcsp@gmail.com>
2020-04-23 05:47:21 -0300

    Testing Some Function on JS

---
+++ Site - Projeto Aps/js/index.js
@@ -0,0 +1,8 @@
+let upper_right = document.getElementById("upper-right")
+function rightside(data){
+   upper_right.innerHTML=`
+    <p> Bom dia </p>
+    <hr>
+    <p> Coversando as <b>05:44h</b> da manhã</p>
+   `
+}
