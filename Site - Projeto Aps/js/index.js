bd092dabeec5ccdff1123d301e9d8db01295882a
MarllonCampos <marllondcsp@gmail.com>
2020-04-30 07:25:30 -0300

    Mudanças de Estilo e JS
    Colocado os panels para troca de "page" na parte inferior, como também ao invés de inserção em HTML pelo js, colocado como display:none as divs que não serão utéis na segunda pagina

--- Site - Projeto Aps/js/index.js
+++ Site - Projeto Aps/js/index.js
@@ -1,67 +1,22 @@
-let upper_right = document.getElementById("upper-right")
+var upper_right = document.getElementById("upper-right")
+var upper_left = document.querySelector('.upper-left')
+var lower_left = document.querySelector('.lower-left')
+var lower_right = document.querySelector('.lower-right')
 let center = document.querySelector(".center")
 var containerContent = document.querySelector(".container-content")
-let leftside_bool = 1
+
 
 
 
 function leftside(sheet){
-    if (leftside_bool == 1){
-    //Condição colocada para que impeça de clicar duas vezes sobre o mesmo botão
-    }else{
         document.querySelector('.pagestyle').setAttribute('href',sheet)
-        document.body.innerHTML= `
-              <div class="container"> 
-                   <div class="panels">
-                       <div class="leftpanel" onclick="">x</div>
-                       <div class="rightpanel" onclick="rightside('css/secondpage.css')">y</div>
-                   </div>
-
-                   <div class="container-content">
-                       <div class='content' id='upper-left'>a</div>    
-
-                       <div class='content'id='lower-left'>b</div>     
-
-                       <div class='content'id='center'>c</div>      
-
-                       <div class='content' id='upper-right'>d</div>      
-
-                       <div class='content' id='lower-right'>e</div>
-                   </div>
-
-               </div> 
-        `
-        leftside_bool = 1
-    }
-   
    
 }
 
 
 function rightside(sheet){
-    leftside_bool = 0
-   document.querySelector('.pagestyle').setAttribute('href',sheet)
-   
-
-   document.body.innerHTML=`
-
-               <div class="container"> 
-
-                  <div class="panels">
-                      <div class="leftpanel" onclick="leftside('css/style.css')">x</div>
-                      <div class="rightpanel">y</div>
-                  </div>
-
-                  <div class="container-content">
-
-                      <div class='content'id='center'>c</div>
-
-                  </div>
-
-               </div>  
-
-      </div>
-`
+    document.querySelector('.pagestyle').setAttribute('href',sheet)
+     
 }
 
 
