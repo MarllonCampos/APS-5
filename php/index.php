21b8b0f36eccfb2b5845746f3c31936caeb574e1
RutyRibeiro <64114456+RutyRibeiro@users.noreply.github.com>
2020-05-02 04:38:41 -0300

    Consulta banco de dados

--- php/index.php
+++ php/index.php
@@ -1,18 +1,28 @@
 <?php
-$Servidor = 'apsunip5.cslcoibcanu8.sa-east-1.rds.amazonaws.com';
-$User='root';
-$Senha='apsardunip';
-$DataBase='aps';
+    $Servidor = 'apsunip5.cslcoibcanu8.sa-east-1.rds.amazonaws.com';
+    $User='root';
+    $Senha='apsardunip';
+    $Database='aps';
+    $num;
 
-$conexão= mysqli_connect($Servidor,$User,$Senha,$DataBase);
-
-if(mysqli_connect_errno($conexão)){
-    echo"Conexão não estabelecida";
-}else{
-        echo"Conexão realizada com sucesso!";}
+    $conexão= mysqli_connect($Servidor,$User,$Senha,$Database) or die ("Conexão não estabelecida!");
 
 
-
-
+    $sql = "select * from TEMPERATURA";
+    $resultado = mysqli_query($conexão,$sql) or die("erro ao consultar banco de dados");
         
-?>
+        while($registro = mysqli_fetch_assoc($resultado))
+        {
+            $id=$registro['id'];
+            $temperatura=$registro['Temperatura'];
+            $data=$registro['Data'];
+            $hora=$registro['Hora'];
+            
+            echo $id;
+            echo $temperatura;
+            echo $data;
+            echo $hora;
+        }
+    mysqli_close($conexão);
+?>    
+        
