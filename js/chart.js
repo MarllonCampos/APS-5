8498e9e41a0da7ef0ccdd0d355ae61b77b3331b8
MarllonCampos <marllondcsp@gmail.com>
2020-05-02 05:19:09 -0300

    Merge to master

--- js/chart.js
+++ js/chart.js
@@ -18,8 +18,3 @@ var grafico = new Chart(ctx,{
 
 const tempo = 2000; //Vinte segundos
 
-fetch('php/index.php') 
-.then((res) => res.json())
-.then(response => {
-    console.log(response)
-}).catch(error => console.log(error))
