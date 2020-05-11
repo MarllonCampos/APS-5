4e7045a4a5b59763a2144a332d47a9c81dc46090
MarllonCampos <marllondcsp@gmail.com>
2020-05-11 15:47:44 -0300

    Troquei o servidor de busca da api, agora o servidor esta diretamente ligado ao servidor da AMAZON(AWS)

--- js/chart.js
+++ js/chart.js
@@ -21,7 +21,7 @@ var grafico = new Chart(ctx,{
 
 var RefreshAutomatico = setInterval(function minhafuncao() {
     $.ajax({
-         url: 'http://localhost:3000/temperatura/',
+         url: 'http://ec2-18-228-194-165.sa-east-1.compute.amazonaws.com:3000/temperatura/',
          dataType: 'json', 
          cache:false,
          type: 'get',
